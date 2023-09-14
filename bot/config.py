2f15220e61c600d2e728689968a3142edc83582b
vaicanho <145003107+vaicanho@users.noreply.github.com>
2023-09-14 21:05:56 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -12,8 +12,8 @@ with open(config_dir / "config.yml", 'r') as f:
 config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
-telegram_token = config_yaml["telegram_token"]
-openai_api_key = config_yaml["openai_api_key"]
+telegram_token = config_yaml["6466018521:AAHMYO0kwSxmToJ7JrVO8BYdUqrsUBIieYY"]
+openai_api_key = config_yaml["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"]
 openai_api_base = config_yaml.get("openai_api_base", None)
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
